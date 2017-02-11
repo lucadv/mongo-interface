943ba55ac0e08c4e507353b8c00ce434eb7fcf75
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2017-02-12 00:55:00 +0100

    uplift libraries

--- test/index.js
+++ test/index.js
@@ -1,63 +1,63 @@
-var Lab = require('lab');
-var Code = require('code');
-var Mongo = require('../lib');
+const Lab = require('lab');
+const Code = require('code');
+const Mongo = require('../lib');
 
-var lab = exports.lab = Lab.script();
-var before = lab.before;
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
-var after = lab.after;
-var describe = lab.describe;
-var it = lab.it;
-var expect = Code.expect;
+const lab = exports.lab = Lab.script();
+const before = lab.before;
+const beforeEach = lab.beforeEach;
+const afterEach = lab.afterEach;
+const after = lab.after;
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
 
-var options = {
+const options = {
   uri: 'mongodb://localhost:27017/startup',
-  collections: [ 'test' ]
+  collections: ['test']
 };
 
-var db = {};
+const db = {};
 
-before(function (done) {
-  Mongo.create(options, function(err, collections) {
+before(done => {
+  Mongo.create(options, (err, collections) => {
     expect(err).to.not.exist();
     db.collections = collections;
     done();
   });
 });
 
-after(function (done) {
-  db.collections.test.remove({}, function (err) {
+after(done => {
+  db.collections.test.remove({}, err => {
     expect(err).to.not.exist();
     done();
   });
 });
 
-describe('Mongo Interface Operations', function () {
+describe('Mongo Interface Operations', () => {
 
-  beforeEach(function (done) {
-    var manyDocs = [
+  beforeEach(done => {
+    const manyDocs = [
       { name: 'doc1' },
       { name: 'doc2' },
       { name: 'doc3' },
       { name: 'doc4' }
     ];
 
-    db.collections.test.insertMany(manyDocs, function (err, result) {
+    db.collections.test.insertMany(manyDocs, (err, result) => {
       expect(err).to.not.exist();
       expect(result.insertedCount).to.equal(4);
       done();
     });
   });
 
-  afterEach(function (done) {
-    db.collections.test.remove({ name: {$in: ['doc1', 'doc2', 'doc3', 'doc4']}}, done);
+  afterEach(done => {
+    db.collections.test.remove({ name: { $in: ['doc1', 'doc2', 'doc3', 'doc4'] } }, done);
   });
 
-  describe('Insert operations', function () {
+  describe('Insert operations', () => {
 
-    it('should insert document', function (done) {
-      db.collections.test.insert({ name: 'a doc'}, function (err, result) {
+    it('should insert document', done => {
+      db.collections.test.insert({ name: 'a doc' }, (err, result) => {
         expect(err).to.not.exist();
         expect(result.insertedCount).to.equal(1);
         done();
@@ -65,19 +65,19 @@ describe('Mongo Interface Operations', function () {
     });
   });
 
-  describe('Update operations', function () {
+  describe('Update operations', () => {
 
-    it('should find one and update it', function (done) {
-      db.collections.test.findOneAndUpdate({ name: 'doc1'}, { name: 'doc1b' }, null, function (err, result) {
+    it('should find one and update it', done => {
+      db.collections.test.findOneAndUpdate({ name: 'doc1' }, { name: 'doc1b' }, null, (err, result) => {
         expect(err).to.not.exist();
         expect(result.ok).to.equal(1);
-        expect(result.lastErrorObject).to.deep.equal({ updatedExisting: true, n: 1 });
+        expect(result.lastErrorObject).to.equal({ updatedExisting: true, n: 1 });
         done();
       });
     });
 
-    it('should update one', function (done) {
-      db.collections.test.updateOne({ name: 'doc1'}, { name: 'doc1b' }, null, function (err, res) {
+    it('should update one', done => {
+      db.collections.test.updateOne({ name: 'doc1' }, { name: 'doc1b' }, null, (err, res) => {
         expect(err).to.not.exist();
         expect(res.result.ok).to.equal(1);
         expect(res.matchedCount).to.equal(1);
@@ -87,24 +87,24 @@ describe('Mongo Interface Operations', function () {
       });
     });
 
-    it('should update many', function (done) {
-      db.collections.test.updateMany({ name: {$in: ['doc1', 'doc2']}}, { $set: { name: 'doc1b'} }, null,
-        function (err, res) {
+    it('should update many', done => {
+      db.collections.test.updateMany({ name: { $in: ['doc1', 'doc2'] } }, { $set: { name: 'doc1b' } }, null,
+        (err, res) => {
           expect(err).to.not.exist();
           expect(res.result.ok).to.equal(1);
           expect(res.matchedCount).to.equal(2);
           expect(res.modifiedCount).to.equal(2);
           expect(res.upsertedCount).to.equal(0);
           done();
-      });
+        });
     });
 
   });
 
-  describe('Find operations', function () {
+  describe('Find operations', () => {
 
-    it('should find inserted documents', function (done) {
-      db.collections.test.find({ name: 'a doc' }, function (err, docs) {
+    it('should find inserted documents', done => {
+      db.collections.test.find({ name: 'a doc' }, (err, docs) => {
         expect(err).to.not.exist();
         expect(docs).to.exist();
         expect(docs).to.have.length(1);
@@ -114,8 +114,8 @@ describe('Mongo Interface Operations', function () {
       });
     });
 
-    it('should find one document', function (done) {
-      db.collections.test.findOne({ name: 'doc1' }, function (err, doc) {
+    it('should find one document', done => {
+      db.collections.test.findOne({ name: 'doc1' }, (err, doc) => {
         expect(err).to.not.exist();
         expect(doc).to.exist();
         expect(doc._id).to.exist();
